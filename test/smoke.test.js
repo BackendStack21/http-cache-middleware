8b4d8eed8bf824f23716d345d1d82e792adf6b94
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-07-02 22:41:10 +0200

    supporting cache-control and etag headers

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -37,8 +37,16 @@ describe('cache middleware', () => {
     server.get('/cache-buffer', (req, res) => {
       setTimeout(() => {
         res.setHeader('x-cache-timeout', '1 minute')
+        res.setHeader('etag', '1')
+        res.setHeader('cache-control', 'no-cache')
         res.send(Buffer.from('world'))
       }, 50)
+    })
+
+    server.get('/cache-control', (req, res) => {
+      res.setHeader('cache-control', 'max-age=60')
+      res.setHeader('etag', '1')
+      res.send('cache')
     })
 
     server.delete('/cache', (req, res) => {
@@ -93,6 +101,35 @@ describe('cache middleware', () => {
   it('cache hit (buffer)', async () => {
     const res = await got('http://localhost:3000/cache-buffer')
     expect(res.body).to.equal('world')
+    expect(res.headers['x-cache-hit']).to.equal('1')
+    expect(res.headers['cache-control']).to.equal('no-cache')
+    expect(res.headers['etag']).to.equal('1')
+  })
+
+  it('cache hit (buffer) - Etag', async () => {
+    const res = await got('http://localhost:3000/cache-buffer')
+    expect(res.body).to.equal('world')
+    expect(res.headers['x-cache-hit']).to.equal('1')
+    expect(res.headers['etag']).to.equal('1')
+  })
+
+  it('cache hit (buffer) - If-None-Match', async () => {
+    const res = await got('http://localhost:3000/cache-buffer', {
+      headers: {
+        'If-None-Match': '1'
+      }
+    })
+    expect(res.statusCode).to.equal(304)
+  })
+
+  it('cache create - cache-control', async () => {
+    const res = await got('http://localhost:3000/cache-control')
+    expect(res.body).to.equal('cache')
+  })
+
+  it('cache hit - cache-control', async () => {
+    const res = await got('http://localhost:3000/cache-control')
+    expect(res.body).to.equal('cache')
     expect(res.headers['x-cache-hit']).to.equal('1')
   })
 
